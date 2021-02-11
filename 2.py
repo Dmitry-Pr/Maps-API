a72db2df052878b963520e65fd4f4d9dcb54776f
dima <muhineima@gmail.com>
2021-02-11 09:55:15 +0300

    ml

--- 2.py
+++ 2.py
@@ -1,92 +1,41 @@
+import cv2
+import numpy as np
 import os
-import sys
-import pygame
-import requests
+
+'''
+ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) - германия
+ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV) - негатив
+ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC) - +белый - белый
+ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO) недогермания
+ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV) - зелёнка'''
+
+hsv = cv2.imread('27.jpg')
+hsv = cv2.cvtColor(hsv, cv2.COLOR_BGR2HSV)
+cv2.imshow('bin', hsv)
+tr = cv2.inRange(hsv, (30, 0,0), (90, 255, 255))
+cv2.imshow('bin', hsv)
+hsv = cv2.cvtColor(hsv, cv2.COLOR_BGR2HSV)
+img = cv2.imread('pedistrain.jpg', cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow('', hsv)
+cv2.waitKey(0)
 
 
-def update_x(x):
-    if x >= 180:
-        x = -1 * (360 - x)
-    if x < -180:
-        x = 360 + x
-    return x
+for file in os.listdir("C:\\Users\\Ялицей\\Desktop\\ml_school-main\\data\\training\\a_unevenness"):
+    original = cv2.imread(file)
+    print(type(original))
+    print(type(tr))
 
+# cap = cv2.VideoCapture(0)
+# running = True
+# while (running):
+#     ret, frame = cap.read()
+#     ret, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_TOZERO_INV)
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     cv2.imshow('Video', frame)
+#     # cv2.imshow('frame',gray)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         running = False
 
-x = 0.0
-y = 0.0
-a = 6
-b = 6
-mode = "sat"
-params = {
-    "ll": str(x) + "," + str(y),
-    "spn": str(a) + "," + str(b),
-    "l": mode
-}
-map_request1 = "https://static-maps.yandex.ru/1.x/"
-# Инициализируем pygame
-response = requests.get(map_request1, params=params)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request1)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-# Переключаем экран и ждем закрытия окна.
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and y + 2 * a < 85:
-                y += 2 * a
-                print(y)
-                params['ll'] = f'{x},{y}'
-                response = requests.get(map_request1, params=params)
-                map_file = "map.png"
-                with open(map_file, "wb") as file:
-                    file.write(response.content)
-            if event.key == pygame.K_DOWN and y - 2 * a > -85:
-                y -= 2 * a
-                print(y)
-                params['ll'] = f'{x},{y}'
-                response = requests.get(map_request1, params=params)
-                map_file = "map.png"
-                with open(map_file, "wb") as file:
-                    file.write(response.content)
-            if event.key == pygame.K_LEFT:
-                x -= 2 * a
-                x = update_x(x)
-                print(x)
-                params['ll'] = f'{x},{y}'
-                response = requests.get(map_request1, params=params)
-                map_file = "map.png"
-                with open(map_file, "wb") as file:
-                    file.write(response.content)
-            if event.key == pygame.K_RIGHT:
-                x += 2 * a
-                x = update_x(x)
-                print(x)
-                params['ll'] = f'{x},{y}'
-                response = requests.get(map_request1, params=params)
-                map_file = "map.png"
-                with open(map_file, "wb") as file:
-                    file.write(response.content)
-
-    # Рисуем картинку, загружаемую из только что созданного файла.
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    pygame.display.flip()
-
-pygame.quit()
-
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
+cap.release()
+cv2.destroyAllWindows()
